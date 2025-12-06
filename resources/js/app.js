da167ea08e5a5ff7c7327b869c8bb5cc998c0cc0
Beto Ronceros <betorlsc@gmail.com>
2025-12-06 11:41:16 -0500

    Style: actualizar estilos y scripts de la aplicación

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+// Trix Editor - Limpiar después de enviar
+document.addEventListener('livewire:init', () => {
+    Livewire.on('trix-reset', () => {
+        document.querySelectorAll('input[type="hidden"][name="message"]').forEach(input => {
+            const editor = input.nextElementSibling;
+            if (editor && editor.tagName === 'TRIX-EDITOR' && editor.editor) {
+                editor.editor.loadHTML('');
+                input.value = '';
+            }
+        });
+    });
+});
